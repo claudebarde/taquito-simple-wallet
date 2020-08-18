8d9036bae3ea46ce1ee9a6e890de836cab4e5453
Claude Barde <25201109+claudebarde@users.noreply.github.com>
2020-08-18 12:43:18 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Taquito Simple Wallet
+
+A simple wallet built to demonstrate how easy it is to build a wallet that connects to a Nano Ledger or the Thanos wallet with Taquito!
---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<div class="wallet">
+  <div class="cards">
+    <div class="card" />
+    <div class="card" />
+    <div class="card" />
+  </div>
+  <div class="wallet-interface">
+    <div class="circle" />
+    <h3>Simple Taquito wallet</h3>
+    <div class="buttons">
+      <button class="button">Connect with Ledger</button>
+      <button class="button">Connect with Thanos</button>
+    </div>
+    <div>
+      <div class="input-with-button">
+        <label>
+          <p>Send tezzies to:</p>
+          <input type="text" class="input-address" />
+        </label>
+        <button class="button">Send</button>
+      </div>
+    </div>
+    <div>
+      <p>Get free FA2 tokens from the faucet</p>
+      <button class="button">Get 100 free tokens!</button>
+    </div>
+  </div>
+</div>
